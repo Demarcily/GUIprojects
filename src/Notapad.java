4d124feea89af2f30d7453a2484e989b96f69e4e
Demarcily <60388907+Demarcily@users.noreply.github.com>
2021-02-16 10:54:20 +0100

    Menu
    Got a menu

--- src/Notapad.java
+++ src/Notapad.java
@@ -1,9 +1,18 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
+import java.awt.event.KeyEvent;
 
 public class Notapad {
   public Notapad() {
+    menuBar = new JMenuBar();
+    menu = new JMenu("File");
+    menu.setMnemonic(KeyEvent.VK_F);
+    menuBar.add(menu);
+
+
+
+
     clearButton.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent actionEvent) {
@@ -24,14 +33,21 @@ public class Notapad {
 
   public static void main(String[] args) {
     JFrame frame = new JFrame("Notapad");
+    Notapad helpme = new Notapad();
     frame.setContentPane(new Notapad().panel1);
+    frame.setJMenuBar(helpme.menuBar);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.pack();
     frame.setVisible(true);
+
+
+
   }
 
   private JPanel panel1;
-  private JButton clearButton;
-  private JButton copyButton;
+  private JButton clearButton,copyButton;
   private JTextArea MainText;
+  private JMenuBar menuBar;
+  private JMenu menu;
+  private JMenuItem Item1;
 }
